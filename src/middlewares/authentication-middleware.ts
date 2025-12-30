90c77187f29466008713f4ff005402182f68b661
Antonela Borgogno <antonelaborgogno@gmail.com>
2025-12-30 20:27:47 -0300

    middleware para autenticación

---
+++ src/middlewares/authentication-middleware.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+const SECRET = process.env.JWT_SECRET || 'Mi_Semilla'
+
+export const verifyToken = (req: Request, res: Response, next: NextFunction) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: "No se proporcionó un token de acceso" });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decoded = jwt.verify(token!, SECRET) as any;
+        req.user = decoded; // Guardar el payload (id, role, email) en la request
+        next();
+    } catch (error) {
+        res.status(401).json({ message: "Token inválido o expirado" });
+    }
+};
